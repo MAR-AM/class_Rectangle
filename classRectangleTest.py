a16701e08b60f351ede6144740d66906c57b7ae7
Mariem_AMZIL <147107786+MAR-AM@users.noreply.github.com>
2023-11-18 22:01:33 +0100

    classRectangleTest.py (✿◡‿◡)

---
+++ classRectangleTest.py
@@ -0,0 +1,18 @@
+from classRectangle import Rectangle 
+
+rec_1 = Rectangle (8,8)
+rec_2 = Rectangle (12,4)
+rec_3 = Rectangle (23,9)
+rec_4 = Rectangle (5,10/2)
+
+print("Rectangle 1")
+rec_1.info()
+
+print("\n Rectangle 2")
+rec_2.info()
+
+print("\n Rectangle 3")
+rec_3.info()
+
+print("\n Rectangle 4")
+rec_4.info()
